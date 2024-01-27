96a899da91caa91e7bf1ad75dc7aa400b98710e4
Howard Yun <haoy2001@gmail.com>
2024-01-27 16:48:05 -0500

    move log out button to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,24 @@
-import { HStack, Text, VStack } from "@chakra-ui/react";
+import { Button, HStack, Text, VStack } from "@chakra-ui/react";
+import { useAuth0 } from "@auth0/auth0-react";
 import { Link, Outlet } from "react-router-dom";
 
 const NavBar = () => {
+  const LogoutButton = () => {
+    const { logout } = useAuth0();
+
+    return <Button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Log Out</Button>;
+  };
   return (
     <VStack h="100vh" w="100vw">
-      <HStack alignItems="center" color="white" bg="#7FD4BC" p="3" width="full" borderBottom="5px outset #7FD4BC">
+      <HStack
+        justifyContent="space-between"
+        alignItems="center"
+        color="white"
+        bg="#7FD4BC"
+        p="3"
+        width="full"
+        borderBottom="5px outset #7FD4BC"
+      >
         <HStack alignItems="baseline" gap={4}>
           <HStack alignItems="baseline" pr={7}>
             <Text as="span" fontSize="2xl">
@@ -14,6 +28,7 @@ const NavBar = () => {
           <Link to={"/signed_in/timeline"}>Timeline</Link>
           <Link to={"/signed_in/create"}>Create</Link>
         </HStack>
+        <LogoutButton />
       </HStack>
       <Outlet />
     </VStack>
--- src/routes/timeline.tsx
+++ src/routes/timeline.tsx
@@ -1,23 +1,8 @@
 import { Text } from "@chakra-ui/react";
-import { useAuth0 } from "@auth0/auth0-react";
 import { useEffect, useState } from "react";
 import { client } from "../main";
 import { Milestones } from "../components/timeline";
 import { MilestonesProps } from "../components/timeline";
-
-const LogoutButton = () => {
-  const { logout } = useAuth0();
-
-  return (
-    <button
-      onClick={() =>
-        logout({ logoutParams: { returnTo: window.location.origin } })
-      }
-    >
-      Log Out
-    </button>
-  );
-};
 
 function Timeline() {
   const [data, setData] = useState<any>();
@@ -37,7 +22,6 @@ function Timeline() {
 
   return (
     <>
-      <LogoutButton />
       <Text>Timeline</Text>
       <Milestones milestones={milestones} />
     </>
