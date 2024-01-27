c71945af445a465dbd10195765716c75f71cefeb
Howard Yun <haoy2001@gmail.com>
2024-01-27 14:00:45 -0500

    kintone client working

--- src/routes/create.tsx
+++ src/routes/create.tsx
@@ -1,6 +1,12 @@
 import { Button, Input, VStack } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { client } from "../main";
 
 function Create() {
+  useEffect(() => {
+    client.record.getAllRecords({ app: 2 }).then((res) => console.log(res));
+  });
+
   return (
     <VStack>
       <Input placeholder="Title" size="md" type="text" />
